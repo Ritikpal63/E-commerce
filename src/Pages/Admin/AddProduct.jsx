7b27edfb9983620ae71f34e1b03f5009ee29a162
Ritik <ritikpal227@gmail.com>
2026-01-17 22:25:06 +0530

    Update Product Page

--- src/Pages/Admin/AddProduct.jsx
+++ src/Pages/Admin/AddProduct.jsx
@@ -2,7 +2,44 @@ import React from 'react'
 
 const AddProduct = () => {
   return (
-    <div>AddProduct</div>
+    <>
+    <div className="container team">
+      <h1 className="text-center my-bg fs-4 text-center text-light rounded-2 py-1">
+        Add Product
+      </h1>
+      <form action="">
+      <div className="row mb-3">
+        <input
+          type="text"
+          className="form-control"
+          name="productName"
+          placeholder="Product Name"
+        />
+      </div>
+      <div className="row mb-3">
+        <input
+          type="text"
+          className="form-control"
+          name="productPrice"
+          placeholder="Product Price"
+        />
+      </div>
+      <div className="row mb-3">
+        <input
+          type="file"
+          className=""
+          name="productPic"
+          placeholder="Bio"
+        />
+      </div>
+      <div className="row mb-3 justify-content-center">
+        <div className="col-md-6">
+        <button type='submit' className="btn btn-primary fs-5 w-100">Submit</button>
+        </div>
+      </div>
+      </form>
+    </div>
+  </>
   )
 }
 
