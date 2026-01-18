9773378f5307d0660c712862d337530c4e8eef94
Ritik <ritikpal227@gmail.com>
2026-01-18 23:44:50 +0530

    Update

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import {Link} from 'react-router-dom'
+// import {ProductContext} from '../Context/ProductContext'
+
 
 const Shop = () => {
+  // const {productDetails} = useContext(ProductContext)
+  const productDetails = JSON.parse(localStorage.getItem("Product Details"))
   return (
     <>
 
@@ -112,21 +116,25 @@ const Shop = () => {
                 </span>
               </Link>
             </div>
-
-            <div className="col-12 col-md-4 col-lg-3 mb-5">
-              <Link className="product-item" to="/">
-                <img
-                  src="images/product-3.png"
-                  className="img-fluid product-thumbnail" alt='pic'
-                />
-                <h3 className="product-title">Ergonomic Chair</h3>
-                <strong className="product-price">$43.00</strong>
-
-                <span className="icon-cross">
-                  <img src="images/cross.svg" className="img-fluid" alt='pic' />
-                </span>
-              </Link>
-            </div>
+            {
+              productDetails
+              ? (
+                <div className="col-12 col-md-4 col-lg-3 mb-5">
+                  <Link className="product-item" to="/admin/product">
+                    <img
+                      src="/images/product-3.png"
+                      className="img-fluid product-thumbnail" alt='pic'
+                    />
+                    <h3 className="product-title">{productDetails.productName}</h3>
+                    <strong className="product-price">${productDetails.productPrice}</strong>
+    
+                    <span className="icon-cross">
+                      <img src="images/cross.svg" className="img-fluid" alt='pic' />
+                    </span>
+                  </Link>
+                </div>)
+              :"Loading..."
+              } 
           </div>
         </div>
       </div>
